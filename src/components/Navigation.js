c572f2d0284a687f02675a20540ab6cc8ab9a98e
John Dao <work@johndao.dev>
2021-04-27 20:05:20 +1000

    preparing for aws conversion

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 // react imports
 import {
     //BrowserRouter as Router, // commented out as it doesn't work on github pages
-    HashRouter as Router,
+    BrowserRouter as Router,
     Switch,
     Route,
     Link,
